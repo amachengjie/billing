64baedc80b01306eb52564588d9e2f3e3ba8b444
chengjie <macj4@lenovo.com>
2020-11-16 20:00:21 +0800

    账单列表新增账单时间列

--- guns-vip-main/src/main/webapp/assets/billing/billingInfo.js
+++ guns-vip-main/src/main/webapp/assets/billing/billingInfo.js
@@ -39,6 +39,7 @@ layui.use(['layer', 'form', 'table', 'ztree', 'laydate', 'admin', 'ax', 'excel',
             {field: 'billing_type', sort: true, title: '账单类型'},
             {field: 'billing_memo', sort: true, title: '账单描述', width: 500},
             {field: 'billing_amount', sort: true, title: '账单总金额'},
+            {field: 'billing_time', sort: true, title: '账单时间'},
             {field: 'create_date', sort: true, title: '创建时间'},
             {field: 'member_name', sort: true, title: '用户名称'}
         ]];
@@ -123,7 +124,7 @@ layui.use(['layer', 'form', 'table', 'ztree', 'laydate', 'admin', 'ax', 'excel',
                             }
                         },
                         series: [{
-                            name: '金额',
+                            name: ' 日期:金额',
                             type: 'bar', //柱状
                             data: data.data.billingAmountList,
                             itemStyle: {
@@ -133,80 +134,6 @@ layui.use(['layer', 'form', 'table', 'ztree', 'laydate', 'admin', 'ax', 'excel',
                             },
                         }]
                     };
-
-
-                    var option = {
-                        tooltip: {
-                            trigger: 'axis',
-                            axisPointer: {
-                                type: 'cross',
-                                crossStyle: {
-                                    color: '#999'
-                                }
-                            }
-                        },
-                        toolbox: {
-                            feature: {
-                                dataView: {show: true, readOnly: false},
-                                magicType: {show: true, type: ['line', 'bar']},
-                                restore: {show: true},
-                                saveAsImage: {show: true}
-                            }
-                        },
-                        legend: {
-                            data: ['蒸发量', '降水量', '平均温度']
-                        },
-                        xAxis: [
-                            {
-                                type: 'category',
-                                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-                                axisPointer: {
-                                    type: 'shadow'
-                                }
-                            }
-                        ],
-                        yAxis: [
-                            {
-                                type: 'value',
-                                name: '水量',
-                                min: 0,
-                                max: 250,
-                                interval: 50,
-                                axisLabel: {
-                                    formatter: '{value} ml'
-                                }
-                            },
-                            {
-                                type: 'value',
-                                name: '温度',
-                                min: 0,
-                                max: 25,
-                                interval: 5,
-                                axisLabel: {
-                                    formatter: '{value} °C'
-                                }
-                            }
-                        ],
-                        series: [
-                            {
-                                name: '蒸发量',
-                                type: 'bar',
-                                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-                            },
-                            {
-                                name: '降水量',
-                                type: 'bar',
-                                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-                            },
-                            {
-                                name: '平均温度',
-                                type: 'line',
-                                yAxisIndex: 1,
-                                data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-                            }
-                        ]
-                    };
-
 
                     chartZhu.setOption(optionchart, true);
                 }
