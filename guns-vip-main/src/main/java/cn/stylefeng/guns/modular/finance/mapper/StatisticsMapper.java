c3d093724c97dd3b9ac177155149d14a00a9e4a6
chengjie <macj4@lenovo.com>
2020-11-24 09:25:02 +0800

    优化实体层参数映射

--- guns-vip-main/src/main/java/cn/stylefeng/guns/modular/finance/mapper/StatisticsMapper.java
+++ guns-vip-main/src/main/java/cn/stylefeng/guns/modular/finance/mapper/StatisticsMapper.java
@@ -1,13 +1,14 @@
 package cn.stylefeng.guns.modular.finance.mapper;
 
 import cn.stylefeng.guns.modular.finance.entity.BillingReport;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
 
 public interface StatisticsMapper {
 
-    List<String> getReportDate(Date date);
+    List<String> getReportDate(@Param("date") Date date);
 
-    List<BillingReport> getReportData(Date date, String type);
+    List<BillingReport> getReportData(@Param("date") Date date, @Param("type") String type);
 }
